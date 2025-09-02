5b256dcaa7d109d7150ccfdd7ad760d389c5294a
Alan Roy <post101box@gmail.com>
2025-09-02 10:50:04 +0530

    Initial commit – completed React TypeScript landing page

---
+++ src/components/Background.tsx
@@ -0,0 +1,15 @@
+/**
+ * Background.tsx
+ * ---------------------
+ * Renders a full-screen gradient background.
+ *
+ * Created: 2025-09-01
+ */
+
+const Background = () => {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-night-top to-night-bottom"></div>
+  );
+};
+
+export default Background;
